e36fca4f9eff947686dbd2f4c5e579bc029b77ae
kcwjy90 <kcwjy90@gmail.com>
2018-09-14 00:19:40 -0700

    ha

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -73,52 +73,39 @@ var constel = [
         name: "Virgo",
         symbol: "♍︎",
         image: "assets/images/Virgo.jpg"
-    }
-//     },
+    },
 
-//     //repeat from here to pull later
-//     {
-//         name: "Taurus",
-//         symbol: "♉︎",
-//         image: "assets/images/Taurus.jpg"
-//     },
+    //repeat from here to pull later
+    {
+        name: "Taurus",
+        symbol: "♉︎",
+        image: "assets/images/Taurus.jpg"
+    },
 
     
-//     {
-//         name: "Aries",
-//         symbol: "♈︎",
-//         image: "assets/images/Aries.jpg"
-//     },
+    {
+        name: "Aries",
+        symbol: "♈︎",
+        image: "assets/images/Aries.jpg"
+    },
 
-//     {
-//         name: "Gemini",
-//         symbol: "♊︎",
-//         image: "assets/images/Gemini.jpg"
-//     },
+    {
+        name: "Gemini",
+        symbol: "♊︎",
+        image: "assets/images/Gemini.jpg"
+    },
 
-//     {
-//         name: "Cancer",
-//         symbol: "♋︎",
-//         image: "assets/images/Cancer.jpg"
-//     },
+    {
+        name: "Cancer",
+        symbol: "♋︎",
+        image: "assets/images/Cancer.jpg"
+    },
 
-//     {
-//         name: "Leo",
-//         symbol: "♈︎",
-//         image: "assets/images/Leo.jpg"
-//     },
-
-//     {
-//         name: "Libra",
-//         symbol: "♎︎",
-//         image: "assets/images/Libra.jpg"
-//     },
-
-//    {
-//         name: "Scorpius",
-//         symbol: "♏︎",
-//         image: "assets/images/Scorpius.jpg"
-//     }
+    {
+        name: "Leo",
+        symbol: "♈︎",
+        image: "assets/images/Leo.jpg"
+    }
 ];
 
 //right answer
@@ -135,6 +122,7 @@ var falseAnswer3;
 var nonAnswerDiv1;
 var nonAnswerDiv2;
 var nonAnswerDiv3;
+var counter = 0;
 
 //to remove roundanswer from constel and put in here.
 var usedAnswer = [];
@@ -144,10 +132,7 @@ var answerDiv;
 //div number. remove div number with the correct answer so false answers can go here.
 var possibleAnswer = [0, 1, 2, 3];
 
-var answerIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-var falseAnswerOption1;
-var falseAnswerOption2;
-var falseAnswerOption3;
+
 //reflecting score on html
 var rightAnswer = 0;
 var wrongAnswer = 0;
@@ -157,89 +142,61 @@ var wrongAnswer = 0;
 gameStart();
 
 function gameStart (){
-    roundAnswer = constel[Math.floor(Math.random()*12)];
+    roundAnswer = constel[Math.floor(Math.random()*3)];
     $(".mainImage").html('<img class="constImage" src="'+roundAnswer.image+'">');
-    
+    console.log(roundAnswer);
     answerDiv = Math.floor(Math.random()*4);
     //this is to compare id that the answer is in with the id the guesser chooses
     roundAnswerIn = $("#answer"+answerDiv).html(roundAnswer.name);
     roundAnswerId = roundAnswerIn[0].id;
     //this is to put false answers into other divs that are not occupied by the answer
     possibleAnswer.splice(answerDiv, 1);
-    //this is to remove right answer into the div usedAnswer for no repeat
-    indexRoundAnswer = constel.indexOf(roundAnswer);
-    usedAnswer = constel.splice(indexRoundAnswer, 1);
-    
-
-    
-    // console.log(indexRoundAnswer);
-    // console.log(usedAnswer);
-    // console.log(constel);
-    
-    // console.log(answerIndex);
   
+    
+    
     if (constel.length > 8) {
-    answerIndex.splice(indexRoundAnswer, 1);
-    answerIndex;
-    // console.log(answerIndex);
-    // console.log(constel);
-    falseAnswerOption1 = Math.floor(Math.random()*answerIndex.length);
-    falseAnswer1 = constel[falseAnswerOption1];
-
-    console.log(falseAnswerOption1);
-    console.log(falseAnswer1);
+    falseAnswer1 = constel[Math.floor(Math.random()*2)+3];
     nonAnswerDiv1 = $("#answer"+possibleAnswer[0]).html(falseAnswer1.name);
-
-    answerIndex.splice(falseAnswer1, 1);
-    answerIndex;
-    falseAnswerOption2 = [Math.floor(Math.random()*answerIndex.length)];
-    falseAnswer2 = constel[falseAnswerOption2];
-
-    // console.log(answerIndex);
-    // console.log(constel);
-    console.log(falseAnswerOption2);
-    console.log(falseAnswer2);
+    falseAnswer2 = constel[Math.floor(Math.random()*2)+5];
     nonAnswerDiv2 = $("#answer"+possibleAnswer[1]).html(falseAnswer2.name);
-
-    answerIndex.splice(falseAnswer2, 1);
-    answerIndex;
-    falseAnswerOption3 = [Math.floor(Math.random()*answerIndex.length)];
-    falseAnswer3 = constel[falseAnswerOption3];
-    // console.log(answerIndex);
-    // console.log(constel);
-    console.log(falseAnswerOption3);
-    console.log(falseAnswer3);
+    falseAnswer3 = constel[Math.floor(Math.random()*2)+7];
     nonAnswerDiv3 = $("#answer"+possibleAnswer[2]).html(falseAnswer3.name);
     } else {
         falseAnswer1 = usedAnswer[Math.floor(Math.random()*2)];
-        console.log(falseAnswer1);
         nonAnswerDiv1 = $("#answer"+possibleAnswer[0]).html(falseAnswer1.name);
         falseAnswer2 = usedAnswer[Math.floor(Math.random()*2)+2];
-        console.log(falseAnswer2);
         nonAnswerDiv2 = $("#answer"+possibleAnswer[1]).html(falseAnswer2.name);
         falseAnswer3 = usedAnswer[Math.floor(Math.random()*2)+4];
-        console.log(falseAnswer3);
         nonAnswerDiv3 = $("#answer"+possibleAnswer[2]).html(falseAnswer3.name);
     };
     //putting used answer so there is no repeat
-    
-    
-    
+    indexRoundAnswer = constel.indexOf(roundAnswer);
+    console.log(usedAnswer);
 
 };
 
 
 $(".answer").on("click", function(){
+
+    counter++;
     
     if ((this).id == roundAnswerId){
         rightAnswer++
         $(".rightCount").html(rightAnswer);
+        usedAnswer = constel.splice(indexRoundAnswer, 1);
         possibleAnswer = [0, 1, 2, 3];
     } else {
         wrongAnswer++
         $(".wrongCount").html(wrongAnswer);
+        usedAnswer = constel.splice(indexRoundAnswer, 1);
         possibleAnswer = [0, 1, 2, 3];
-    }
+    };
+
+    if(counter === 7){
+        alert("good job! you got "+rightAnswer+" points!!!")
+        alert("Let's Play Again")
+        window.location.reload();
+    };
 
     gameStart ();
 
@@ -295,3 +252,65 @@ $(".answer").on("click", function(){
 //     roundAnswerClass = roundAnswerIn[0];
 //     console.log(roundAnswerIn);
 //     console.log(roundAnswerClass);
+
+// function gameStart (){
+//     roundAnswer = constel[Math.floor(Math.random()*constel.length)];
+//     $(".mainImage").html('<img class="constImage" src="'+roundAnswer.image+'">');
+    
+//     answerDiv = Math.floor(Math.random()*4);
+//     //this is to compare id that the answer is in with the id the guesser chooses
+//     roundAnswerIn = $("#answer"+answerDiv).html(roundAnswer.name);
+//     roundAnswerId = roundAnswerIn[0].id;
+//     //this is to put false answers into other divs that are not occupied by the answer
+//     possibleAnswer.splice(answerDiv, 1);
+//     //this is to remove right answer into the div usedAnswer for no repeat
+//     indexRoundAnswer = constel.indexOf(roundAnswer);
+//     usedAnswer = constel.splice(indexRoundAnswer, 1);
+    
+
+    
+//     // console.log(indexRoundAnswer);
+//     // console.log(usedAnswer);
+//     // console.log(constel);
+    
+//     // console.log(answerIndex);
+  
+//     if (constel.length > 4) {
+//     answerIndex.splice(indexRoundAnswer, 1);
+//     answerIndex;
+  
+//     falseAnswerOption1 = answerIndex[0];
+//     falseAnswer1 = constel[falseAnswerOption1];
+
+//     nonAnswerDiv1 = $("#answer"+possibleAnswer[0]).html(falseAnswer1.name);
+
+//     answerIndex.splice(falseAnswerOption1, 1);
+//     answerIndex;
+//     falseAnswerOption2 = answerIndex[1];
+//     falseAnswer2 = constel[falseAnswerOption2];
+
+//     nonAnswerDiv2 = $("#answer"+possibleAnswer[1]).html(falseAnswer2.name);
+
+//     answerIndex.splice(falseAnswerOption2, 1);
+//     answerIndex;
+//     falseAnswerOption3 = answerIndex[2];
+//     falseAnswer3 = constel[falseAnswerOption3];
+
+//     nonAnswerDiv3 = $("#answer"+possibleAnswer[2]).html(falseAnswer3.name);
+//     } else {
+//         falseAnswer1 = usedAnswer[Math.floor(Math.random()*2)];
+//         console.log(falseAnswer1);
+//         nonAnswerDiv1 = $("#answer"+possibleAnswer[0]).html(falseAnswer1.name);
+//         falseAnswer2 = usedAnswer[Math.floor(Math.random()*2)+2];
+//         console.log(falseAnswer2);
+//         nonAnswerDiv2 = $("#answer"+possibleAnswer[1]).html(falseAnswer2.name);
+//         falseAnswer3 = usedAnswer[Math.floor(Math.random()*2)+4];
+//         console.log(falseAnswer3);
+//         nonAnswerDiv3 = $("#answer"+possibleAnswer[2]).html(falseAnswer3.name);
+//     };
+//     //putting used answer so there is no repeat
+    
+    
+    
+
+// };
